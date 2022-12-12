3462aa38fb2631b0c7c400b4b4724408c7749f5d
Kundan Kumar <kkundan.iitr@gmail.com>
2022-12-13 01:55:29 +0530

    changed typo

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -77,7 +77,7 @@ function Projects() {
     <Layout title="Projects" description={siteConfig.tagline}>
       <header className={styles.projectPageHeader}>
         <h2 className="border-0 border-b-4 border-solid border-success">
-          My projects
+          My Projects
         </h2>
       </header>
       <main ref={mainRef} hidden={true}>
