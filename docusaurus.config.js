0ca3fcd1c0c96f189839129f4a8126c3b31764d8
kundan-at-100258282066 <kkundan.iitr@gmail.com>
2022-12-13 01:43:11 +0530

    initial commit for portfolio

---
+++ docusaurus.config.js
@@ -0,0 +1,130 @@
+const math = require("remark-math");
+const katex = require("rehype-katex");
+
+const internetProfiles = {
+  linkedin: {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/kundan-kumar-77a613a9/",
+  },
+  github: {
+    label: "GitHub",
+    href: "https://github.com/kundank78",
+  },
+  email: {
+    label: "Email",
+    href: "mailto:kkundan.iitr@gmail.com",
+  },
+  blog: {
+    label: "Blog",
+    to: "blog",
+  },
+  projects: {
+    label: "Projects",
+    to: "projects",
+  },
+  resume: {
+    label: "Resume",
+    href: "https://www.linkedin.com/in/kundan-kumar-77a613a9/overlay/1635493534947/single-media-viewer/",
+  },
+};
+
+module.exports = {
+  title: "Kundan Kumar",
+  tagline:
+    "I am a Software Engineer and Cloud Engineer passionate about solving meaningful problem.",
+  url: "https://kundank78.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  favicon: "img/logo.png",
+  organizationName: "kundank78",
+  projectName: "kundank78.github.io",
+  deploymentBranch: "gh-pages",
+  themeConfig: {
+    colorMode: {
+      defaultMode: "dark",
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
+    navbar: {
+      hideOnScroll: true,
+      title: "Kundan Kumar",
+      logo: {
+        alt: "Kundan Kumar",
+        src: "img/logo.png",
+        target: "_self",
+      },
+      items: [
+        { to: "blog/", label: "Blog", position: "left" },
+        { to: "projects/", label: "Projects", position: "left" },
+        {
+          href: "https://kundan.dev/pdf/resume.pdf",
+          label: "Resume",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      links: [
+        {
+          title: "Connect",
+          items: [
+            internetProfiles.linkedin,
+            internetProfiles.github,
+            internetProfiles.email,
+          ],
+        },
+        {
+          title: "Discover",
+          items: [
+            internetProfiles.blog,
+            internetProfiles.projects,
+            internetProfiles.resume,
+          ],
+        },
+      ],
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          disableVersioning: false,
+          editCurrentVersion: false,
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+//          editUrl: "https://github.com/DigiPie/kaya-folio/tree/main/website/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+  stylesheets: [
+    {
+      href: "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css",
+      type: "text/css",
+      integrity:
+        "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X",
+      crossorigin: "anonymous",
+    },
+  ],
+  plugins:    [
+    async function tailwindPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import Link from "@docusaurus/Link";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+
+import styles from "./styles.module.css";
+import SocialLinks from "./components/_SocialLinks";
+
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+
+  const mainRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(true)
+  const [headerHeight, setHeaderHeight] = useState(1536);
+  const [bannerHeight, setBannerHeight] = useState(256);
+
+  useEffect(() => {
+    const tempHeaderHeight = Math.max(384, window.innerHeight);
+    setHeaderHeight(tempHeaderHeight);
+    setBannerHeight(Math.max(256, tempHeaderHeight / 2));
+    setIsLoading(false);
+    mainRef.current.hidden = false;
+  }, []);
+
+  return (
+    <Layout title="Home" description={siteConfig.tagline}>
+      <header className={styles.heroBanner} style={{ minHeight: headerHeight }}>
+        <div
+          className={styles.heroBannerWrapper}
+          style={{ minHeight: bannerHeight, display: isLoading ? "none" : "block" }}
+        >
+          <p>Hi, my name is</p>
+          <h1 className="text-success">Kundan Kumar.</h1>
+          <p>
+            I am a <span className="text-warning">Software Engineer</span> and{" "}
+            <span className="text-danger">Cloud Engineer</span> passionate about
+            solving meaningful problems.
+          </p>
+          <SocialLinks />
+          <p>
+            <Link to={useBaseUrl("#main")}>
+              <button className="border-0 rounded p-2 pl-4 pr-0 bg-primary-900 hover:bg-primary-800 transition text-white text-lg cursor-pointer">
+                whoami<span className="pl-1 animate-pulse">▎</span>
+              </button>
+            </Link>
+          </p>
+        </div>
+      </header>
+      <main id="main" ref={mainRef} hidden={true}>
+      <div className={styles.aboutHeader}>
+        <h2 className="border-0 border-b-4 border-solid border-success">Who am I</h2>
+      </div>
+       <div className={styles.about}>
+          <div>
+            <img
+              className={styles.aboutProfilePic}
+              src={useBaseUrl("img/profilepic.jpg")}
+            />
+          </div>
+          <div class={styles.aboutText}>
+            <h2>Hi</h2>
+            <p>
+              I am Kundan, a Software Engineer with 3 years of experience in software development. I am currently exploring Kubernetes & public cloud domain.
+            </p>
+            <p>
+              🚀 I love experimenting with tools & languages. Please checkout my recent  {" "}
+              <Link to={useBaseUrl("projects/")}>
+                projects
+              </Link>
+              .
+            </p>
+            <p>
+              👨‍💻 I have been writing code at <a href="https://jpmorgan.com">JP Morgan</a>{" "}
+              since 2020.
+            </p>
+            <p>
+              🎒 I was a{" "}
+              <a href="https://www.comp.nus.edu.sg/programmes/ug/cs/">
+                Electronics & Communication
+              </a>{" "}
+              major at the{" "}
+              <a href="https://www.comp.nus.edu.sg/">
+                IIT Roorkee
+              </a>{" "}
+              from 2016 to 2020.
+            </p>
+            <p>
+              When not coding, I like to travel & trek to adventurous places.
+            </p>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
